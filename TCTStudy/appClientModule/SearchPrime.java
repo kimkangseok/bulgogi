b4b0a6089ba43ea9666762f70d21005466f1767b
kangskim <kangskim@LAPTOP-KMB6N0AU>
2020-07-02 17:38:18 +0300

    StudyHash01

---
+++ TCTStudy/appClientModule/SearchPrime.java
@@ -0,0 +1,34 @@
+
+public class SearchPrime {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String arg1 = "17";
+		//1, 7, 17, 
+		
+		solutionPrime Sol = new solutionPrime();
+		System.out.println(Sol.solution(arg1));
+				
+
+	}
+
+}
+
+class solutionPrime {
+    public int solution(String numbers) {
+    	int answer = 0;
+    	char[] chNumArray = new char[] {};
+    	chNumArray = numbers.toCharArray();
+    	int numCnt = 0;
+    	for(int i=0; i < chNumArray.length; i++)
+    	{
+    		
+    	}
+    	return answer;
+    }
+    
+    public int calPactorials(int factor)
+    {
+    	
+    }
+}
